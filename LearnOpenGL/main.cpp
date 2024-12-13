b97e78c8ec1e7a91e0aaf894a64450ba234f0504
Frédéric Larivé <frederic.larive@ubisoft.com>
2024-12-13 17:13:11 +0100

    Update main.cpp

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -10,6 +10,8 @@ const int FRAME_DELAY = 1000 / TARGET_FPS; // in milliseconds
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void initDisplayTriangle();
+void loopRenderTriangle();
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -30,6 +32,8 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "{\n"
 "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\n\0";
+
+
 
 
 
@@ -71,6 +75,65 @@ int main()
 
 
 
+    initDisplayTriangle();
+
+
+
+    // render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+
+        // input
+        processInput(window);
+
+        // render a black background
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        // clear previous frame rendered
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        loopRenderTriangle();
+
+
+        
+
+
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        auto end_time = std::chrono::high_resolution_clock::now();
+        std::this_thread::sleep_for(std::chrono::milliseconds(FRAME_DELAY) - (end_time - start_time));
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+void initDisplayTriangle()
+{
     // test shaders
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
@@ -160,60 +223,15 @@ int main()
         (void*)0 // offset in buffer
     );
     glEnableVertexAttribArray(0);
-
-
-
-    // render loop
-    while (!glfwWindowShouldClose(window))
-    {
-        auto start_time = std::chrono::high_resolution_clock::now();
-
-        // input
-        processInput(window);
-
-        // render a black background
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
-        // clear previous frame rendered
-        glClear(GL_COLOR_BUFFER_BIT);
-
-
-        // draw our first triangle
-        glUseProgram(shaderProgram);
-        //glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        // glBindVertexArray(0); // no need to unbind it every time 
-
-
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
-        auto end_time = std::chrono::high_resolution_clock::now();
-        std::this_thread::sleep_for(std::chrono::milliseconds(FRAME_DELAY) - (end_time - start_time));
-    }
-
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    glfwTerminate();
-    return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void loopRenderTriangle()
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
+    // draw our first triangle
+    //glUseProgram(shaderProgram);
+    //glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+    //glDrawArrays(GL_TRIANGLES, 0, 6);
+    //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    // glBindVertexArray(0); // no need to unbind it every time 
 }

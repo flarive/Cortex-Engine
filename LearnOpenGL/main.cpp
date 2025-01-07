94443cd2ec3d285eb9a2965fe577d1051cf1d099
Frédéric Larivé <frederic.larive@ubisoft.com>
2025-01-07 17:53:38 +0100

    Update main.cpp

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -74,6 +74,11 @@ int main()
     
 
     // ..:: Initialization code :: ..
+
+
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
     
     // enable z buffer (depth test)
     glEnable(GL_DEPTH_TEST);
@@ -207,6 +212,10 @@ int main()
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // GL_FILL
 
 
+     // pass projection matrix to shader (as projection matrix rarely changes there's no need to do this per frame)
+    // -----------------------------------------------------------------------------------------------------------
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    ourShader.setMat4("projection", projection);
 
     // render loop
     while (!glfwWindowShouldClose(window))
@@ -226,7 +235,7 @@ int main()
 
 
 
-        // bind Texture
+        // bind textures on corresponding texture units
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE1);
@@ -234,31 +243,20 @@ int main()
 
         
 
-        // be sure to activate the shader
+        // activate shader
         ourShader.use();
 
-        // create transformations
-        //glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-
-        // make sure to initialize matrix to identity matrix first
-        glm::mat4 view = glm::mat4(1.0f);
-        glm::mat4 projection = glm::mat4(1.0f);
 
 
-        
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-
-
-
-        // pass transformation matrices to the shader
-        // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
-        ourShader.setMat4("projection", projection);
+        // camera/view transformation
+        glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        float radius = 10.0f;
+        float camX = static_cast<float>(sin(glfwGetTime()) * radius);
+        float camZ = static_cast<float>(cos(glfwGetTime()) * radius);
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         ourShader.setMat4("view", view);
 
-
-        // render box
+        // render boxes
         glBindVertexArray(VAO);
         for (unsigned int i = 0; i < 10; i++)
         {

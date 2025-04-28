c33b0d6d2e72feea6d0234b08640f472056e910a
Frédéric Larivé <frederic.larive@ubisoft.com>
2025-04-28 17:48:38 +0200

    Trying to add spdlog lib

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "app/myscene6.h"
 #include "app/myscene7.h"
 
+//#include "spdlog/spdlog.h"
 
 // make it easier to switch between apps
 using MyApp = MyApp1;
@@ -44,6 +45,28 @@ static void gamepadUpdate();
 // Startup method
 int main(int, char**)
 {
+    //spdlog::info("Welcome to spdlog!");
+    //spdlog::error("Some error message with arg: {}", 1);
+
+    //spdlog::warn("Easy padding in numbers like {:08d}", 12);
+    //spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    //spdlog::info("Support for floats {:03.2f}", 1.23456);
+    //spdlog::info("Positional args are {1} {0}..", "too", "supported");
+    //spdlog::info("{:<30}", "left aligned");
+
+    //spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+    //spdlog::debug("This message should be displayed..");00
+
+    //// change log pattern
+    //spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+
+    //// Compile time log levels
+    //// Note that this does not change the current log level, it will only
+    //// remove (depending on SPDLOG_ACTIVE_LEVEL) the call on the release code.
+    //SPDLOG_TRACE("Some trace message with param {}", 42);
+    //SPDLOG_DEBUG("Some debug message");
+
+
     app = new MyApp("MyApp", 1280, 720, false);
     if (app)
     {

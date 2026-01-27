c4f4e5aa5c1294b9e0ffc06779f85d6c835596d1
Eviral <flarive@hotmail.com>
2026-01-27 22:01:53 +0100

    Ceramic and porcelain scene

--- src/app/myscene12.cpp
+++ src/app/myscene12.cpp
@@ -8,10 +8,10 @@ MyScene12::MyScene12(string _title, App* _app) : Scene(_title, _app, SceneSettin
     {
         .method = RenderMethod::PBR,
         .HDRSkyboxHide = false,
-        .HDRSkyboxFilePath = "textures/hdr/newport_loft.hdr",
+        .HDRSkyboxFilePath = "textures/hdr/blue_photo_studio_2k.hdr",
         .shadowIntensity = 1.5f,
-        .iblDiffuseIntensity = 1.0f,
-        .iblSpecularIntensity = 1.0f,
+        .iblDiffuseIntensity = 0.2f,
+        .iblSpecularIntensity = 0.1f,
         .enableGammaCorrection = true
     })
 {
@@ -25,25 +25,33 @@ MyScene12::MyScene12(string _title, App* _app) : Scene(_title, _app, SceneSettin
 void MyScene12::init()
 {
     // camera
-    auto trsCamera1 = Transform{ {0.0f, 1.0f, 5.0f} };
-    auto camera1 = make_shared<FlyCamera>(20.0f, -90.0f, -7.0f, 10.0f);
+    auto trsCamera1 = Transform{ { 0.0f, -12.0f, 2.0f } };
+    auto camera1 = make_shared<FpsCamera>(45.0f, -90.0f, 0.0f, 10.0f);
     auto entityCamera1 = make_shared<Entity>("Camera1");
     entityCamera1->addComponent<TransformComponent>(trsCamera1);
     entityCamera1->addComponent<CameraComponent>(camera1);
     getEntityManager().addChild(entityCamera1);
-    
 
 
-    // light
-    auto trsLight1 = Transform{ {0.5f, 1.5f, 3.0f} };
-    auto light1 = make_shared<SpotLight>();
-    light1->intensity = 6.0f;
-    light1->cutoff = 12.0f;
-    light1->outerCutoff = 48.0f;
-    light1->target = vec3(0.0f, 0.0f, 0.0f);
-    light1->ambientColor = Color(0.2f);
-    light1->diffuseColor = Color(1.0f);
-    light1->specularColor = Color(1.0f);
+
+    // lights
+    auto trsLight5 = Transform{ { 0.0f, 4.0f, -2.0f } };
+    auto light5 = make_shared<SpotLight>();
+    light5->intensity = 10.0f;
+    light5->cutoff = 12.5f;
+    light5->outerCutoff = 27.5f;
+    light5->target = vec3(0.0f, 0.0f, -4.0f);
+    auto entityLight5 = make_shared<Entity>("Light5");
+    entityLight5->addComponent<TransformComponent>(trsLight5);
+    entityLight5->addComponent<LightComponent>(light5);
+    getEntityManager().addChild(entityLight5);
+
+
+
+    auto trsLight1 = Transform{ { -10.0f, 10.0f, 10.0f } };
+    auto light1 = make_shared<PointLight>();
+    light1->intensity = 10.0f;
+    light1->diffuseColor = Color(0.8f, 0.2f, 0.1f, 1.0f);
     auto entityLight1 = make_shared<Entity>("Light1");
     entityLight1->addComponent<TransformComponent>(trsLight1);
     entityLight1->addComponent<LightComponent>(light1);
@@ -51,15 +59,49 @@ void MyScene12::init()
 
 
 
+    auto trsLight2 = Transform{ { 10.0f, 10.0f, 10.0f } };
+    auto light2 = make_shared<PointLight>();
+    light2->intensity = 10.0f;
+    auto entityLight2 = make_shared<Entity>("Light2");
+    entityLight2->addComponent<TransformComponent>(trsLight2);
+    entityLight2->addComponent<LightComponent>(light2);
+    getEntityManager().addChild(entityLight2);
+
+
+
+    auto trsLight3 = Transform{ { -10.0f, -10.0f, 10.0f } };
+    auto light3 = make_shared<PointLight>();
+    light3->intensity = 10.0f;
+    auto entityLight3 = make_shared<Entity>("Light3");
+    entityLight3->addComponent<TransformComponent>(trsLight3);
+    entityLight3->addComponent<LightComponent>(light3);
+    getEntityManager().addChild(entityLight3);
+
+
+
+    auto trsLight4 = Transform{ { 10.0f, -10.0f, 10.0f } };
+    auto light4 = make_shared<PointLight>();
+    light4->intensity = 10.0f;
+    auto entityLight4 = make_shared<Entity>("Light4");
+    entityLight4->addComponent<TransformComponent>(trsLight4);
+    entityLight4->addComponent<LightComponent>(light4);
+    getEntityManager().addChild(entityLight4);
+
+
+
+
     // ground
     auto myPlane = make_shared<Plane>();
-    myPlane->setup(make_shared<PBRMaterial>(Color(0.1f),
+    auto matPlane = make_shared<PBRMaterial>(Color(0.2f),
         "textures/pbr/painted-wood/PaintedWood_Color.jpg",
         "textures/pbr/painted-wood/PaintedWood_Normal.jpg",
         "textures/pbr/painted-wood/PaintedWood_Metallic.jpg",
         "textures/pbr/painted-wood/PaintedWood_Roughness.jpg",
-        "textures/pbr/painted-wood/PaintedWood_AmbientOcclusion.jpg"), UvMapping(1.0f));
-    auto trsPlane = Transform(vec3(0.0f, -0.5f, 0.0f), vec3(3.0f), vec3(0.0f));
+        "textures/pbr/painted-wood/PaintedWood_AmbientOcclusion.jpg",
+        "textures/pbr/painted-wood/PaintedWood_Displacement.jpg");
+    matPlane->setNormalIntensity(1.0f);
+    myPlane->setup(matPlane, UvMapping(2.0f));
+    auto trsPlane = Transform(vec3(0.0f, -15.0f, -15.0f), vec3(12.0f), vec3(0.0f, 0.0f, 0.0f));
     auto entityPlane = make_shared<Entity>("MyPlane");
     entityPlane->addComponent<TransformComponent>(trsPlane);
     entityPlane->addComponent<PrimitiveComponent>(myPlane);
@@ -67,31 +109,51 @@ void MyScene12::init()
 
 
 
-    // sphere 1
-    auto mySphere = make_shared<Sphere>();
-    mySphere->setup(make_shared<PBRMaterial>(Color(0.1f),
+    // sphere models
+    auto sphere1 = make_shared<Sphere>();
+
+    auto matSphere1 = make_shared<PBRMaterial>(Color(0.1f, 0.7f, 0.3f, 1.0f),
+        "textures/pbr/ceramic/ClayCeramicGlossy_BaseColor.jpg",
+        "textures/pbr/ceramic/ClayCeramicGlossy_Normal.jpg",
+        "textures/pbr/ceramic/ClayCeramicGlossy_Metallic.jpg",
+        "textures/pbr/ceramic/ClayCeramicGlossy_Roughness.jpg",
+        "textures/pbr/ceramic/ClayCeramicGlossy_AmbientOcclusion.jpg",
+        "textures/pbr/ceramic/ClayCeramicGlossy_Displacement.jpg");
+    matSphere1->setNormalIntensity(1.0f);
+
+    sphere1->setup(matSphere1, UvMapping(1.0f));
+
+    auto trsSphere1 = Transform(vec3(0.0f, -14.0f, -10.0f), vec3(1.0f));
+    auto entitySphere1 = make_shared<Entity>("MySphere1");
+    entitySphere1->addComponent<TransformComponent>(trsSphere1);
+    entitySphere1->addComponent<PrimitiveComponent>(sphere1);
+    getEntityManager().addChild(entitySphere1);
+
+
+
+    auto sphere2 = make_shared<Sphere>();
+
+    auto matSphere2 = make_shared<PBRMaterial>(Color(0.1f),
         "textures/pbr/porcelain/Porcelain_Color.png",
         "textures/pbr/porcelain/Porcelain_Normal.png",
         "textures/pbr/porcelain/Porcelain_Metallic.png",
-        "textures/pbr/porcelain/Porcelain_Roughness.png"), UvMapping(1.0f));
-    auto trsSphere = Transform(vec3(0.0f, -0.35f, 0.0f), vec3(0.2f));
-    auto entitySphere = make_shared<Entity>("MySphere1");
-    entitySphere->addComponent<TransformComponent>(trsSphere);
-    entitySphere->addComponent<PrimitiveComponent>(mySphere);
-    getEntityManager().addChild(entitySphere);
+        "textures/pbr/porcelain/Porcelain_Roughness.png",
+        "textures/pbr/porcelain/Porcelain_AmbientOcclusion.png",
+        "textures/pbr/porcelain/Porcelain_Displace.png", 2.0f);
+    matSphere2->setNormalIntensity(1.0f);
 
+    sphere2->setup(matSphere2, UvMapping(1.0f));
 
-
-    textFPSCount.setup(app->window, FONT_PATH, 28);
-    textPolyCount.setup(app->window, FONT_PATH, 28);
-    textMeshCount.setup(app->window, FONT_PATH, 28);
-    textPrimitiveCount.setup(app->window, FONT_PATH, 28);
-
-    textDrawnCount.setup(app->window, FONT_PATH, 28);
-    textTotalCount.setup(app->window, FONT_PATH, 28);
+    auto trsSphere2 = Transform(vec3(3.0f, -14.0f, -10.0f), vec3(1.0f));
+    auto entitySphere2 = make_shared<Entity>("MySphere2");
+    entitySphere2->addComponent<TransformComponent>(trsSphere2);
+    entitySphere2->addComponent<PrimitiveComponent>(sphere2);
+    getEntityManager().addChild(entitySphere2);
 }
 
 
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
 void MyScene12::key_callback(int key, int scancode, int action, int mods)
 {
     Scene::key_callback(key, scancode, action, mods);
@@ -120,15 +182,19 @@ void MyScene12::key_callback(int key, int scancode, int action, int mods)
         getActiveCamera()->processKeyboard(BACKWARD, deltaTime);
 }
 
+
 void MyScene12::mouse_callback(double xposIn, double yposIn)
 {
+    //(void)xposIn;   //Do nothing
+    //(void)yposIn;   //Do nothing
+
     Scene::mouse_callback(xposIn, yposIn);
 
     if (is_editor_mode || show_demo_window)
         return;
 
-    float xpos{ static_cast<float>(xposIn) };
-    float ypos{ static_cast<float>(yposIn) };
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
 
     if (firstMouse)
     {
@@ -137,8 +203,8 @@ void MyScene12::mouse_callback(double xposIn, double yposIn)
         firstMouse = false;
     }
 
-    float xoffset{ xpos - lastX };
-    float yoffset{ lastY - ypos }; // reversed since y-coordinates go from bottom to top
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
 
     lastX = xpos;
     lastY = ypos;
@@ -165,29 +231,16 @@ void MyScene12::framebuffer_size_callback(int newWidth, int newHeight)
 
 void MyScene12::update(Shader& shader)
 {
+    // draw scene and UI in framebuffer
     (void)shader;   //Do nothing
 }
 
 void MyScene12::updateUI()
 {
-    // render HUD / UI
-    textFPSCount.draw(format("{:.0f} FPS", framerate), 25.0f, 25.0f, 1.0f, vec3(1.0f));
-    textPolyCount.draw(format("{} polys", polycount), app->width - 250.0f, 25.0f, 1.0f, vec3(1.0f));
-    textMeshCount.draw(format("{} meshes", meshcount), app->width - 450.0f, 25.0f, 1.0f, vec3(1.0f));
-    textPrimitiveCount.draw(format("{} primitives", primitivecount), app->width - 650.0f, 25.0f, 1.0f, vec3(1.0f));
 
-    textDrawnCount.draw(format("{} drawn", inFrustrumCount), 25.0f, 120.0f, 1.0f, vec3(1.0f));
-    textTotalCount.draw(format("{} total", totalFrustrumCount), 25.0f, 160.0f, 1.0f, vec3(1.0f));
 }
 
 void MyScene12::clean()
 {
     // clean up any resources
-    textFPSCount.clean();
-    textPolyCount.clean();
-    textMeshCount.clean();
-    textPrimitiveCount.clean();
-    textDrawnCount.clean();
-    textTotalCount.clean();
 }
-
